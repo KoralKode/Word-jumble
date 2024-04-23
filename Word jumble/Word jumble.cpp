5864c7124752cd6f1a621f0d99e44121d580ef39
Stepan Pushkarev <stepa.pushka@outlook.com>
2024-04-23 23:30:32 +0300

    Добавьте файлы проекта.

---
+++ Word jumble/Word jumble.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+    srand(time(NULL));
+    cout << "Расставьте правильно буквы!"<<endl;
+    cout << "Введите <подсказка> если хотите подсказку."<<endl;
+    cout << "Введите <ответ> если сдаётесь." << endl;
+    
+    string words_base[5][2] = { {"слово","В названии игры проверь!"},
+        {"клавиатура","С помощью чего ты напишешь ответ?"},
+        {"автобус","Там много бабушек днём."},
+        {"мясорубка","Что-то делает с мясом."},
+        {"игра","Это что?"} };
+    int words_number = 4;
+    string theword = words_base[words_number][0];
+    for (int i = 0;i < theword.size();++i) {
+        int t = rand() % theword.size();
+        if (i != t) {
+            char s = theword[t];
+            theword[t] = theword[i];
+            theword[i] = s;
+        }
+    }
+    cout << "Угадай: "<<theword<<"\t-\t";
+    
+    cin >> theword;
+    if (theword == "подсказка") {
+        cout << words_base[words_number][1]<<endl;
+        cin >> theword;
+    }
+    if (theword == "ответ") {
+        cout << words_base[words_number][0]<<endl;
+    }
+    if (theword == words_base[words_number][0]) {
+        cout << "Вы абсолютно правы!";
+    }
+    else {
+        cout << "В следующий раз повезёт!";
+    }
+
+}
+
